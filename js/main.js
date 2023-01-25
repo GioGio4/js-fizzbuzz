4dce2198b317c241df0dd779692a2856800d7b39
GioGio4 <g.ceccobelli@outlook.it>
2023-01-25 14:01:10 +0100

    Init project

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// TRACCIA
+// 1 - Scrivi un programma che stampi in console i numeri da 1 a 100,
+// 2 - Ma che per i multipli di 3 stampi in console "Fizz" al posto del
+//     numero e per i multipli di 5 stampi "Buzz".
+// 3-  Per i numeri che sono sia multipli di 3 che di 5 stampi in console "FizzBuzz".
+
